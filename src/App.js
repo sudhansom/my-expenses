cd48d82debc6dd7892dd056c6bb304940768127f
bhuwan <bkspoudel@gmail.com>
2023-10-02 19:12:50 +0200

    filter the expenses on year

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 import "./App.css";
@@ -5,38 +7,58 @@ import Card from "./components/UI/Card";
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpenseFilter from "./components/Expenses/ExpenseFilter";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-  const getData = (expense) => {
-    console.log("My app: ", expense);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const getData = (exp) => {
+    const id = "e" + expenses.length + 1;
+    setExpenses((prev) => {
+      return [...prev, { ...exp, id: id }];
+    });
+  };
+  const getSelectedYear = (year) => {
+    let filteredExpenses = [];
+    setExpenses((prev) => {
+      filteredExpenses = prev.filter((item) => {
+        return item.date.getFullYear().toString() === year.toString();
+      });
+      return filteredExpenses;
+    });
   };
   return (
-    <Card className="expenses">
+    <>
       <NewExpense onGetData={getData} />
-      <ExpenseFilter />
-      {expenses.length &&
-        expenses.map((item) => <ExpenseItem id={item.id} item={item} />)}
-    </Card>
+      <Card id="card1" className="expenses">
+        <ExpenseFilter id="exp-1" onSelectedYear={getSelectedYear} />
+        {expenses.length ? (
+          expenses.map((item) => <ExpenseItem key={item.id} item={item} />)
+        ) : (
+          <p>no item to display.</p>
+        )}
+        {}
+      </Card>
+    </>
   );
 };
 
--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ExpenseFilter = () => {
+import "./ExpenseFilter.css";
+import Card from "../UI/Card";
+
+const ExpenseFilter = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+  let hidenYears = "2022, 2023, 2024";
+  if (selectedYear === "2022") {
+    hidenYears = "2021, 2023, 2024";
+  } else if (selectedYear === "2023") {
+    hidenYears = "2021, 2022, 2024";
+  } else if (selectedYear === "2024") {
+    hidenYears = "2021, 2022, 2023";
+  }
+
+  const changeHandler = (e) => {
+    setSelectedYear(e.target.value);
+    props.onSelectedYear(e.target.value);
+  };
   return (
-    <div className="expenses-filter">
+    <Card className="expenses-filter">
       <div className="expenses-filter__control">
-        <label> </label>
-        <select>
+        <label> Filter by year</label>
+        <select onChange={changeHandler}>
+          <option value="2020">2020</option>
+          <option value="2021">2021</option>
           <option value="2022">2022</option>
-          <option value="2022">2022</option>
-          <option value="2022">2022</option>
-          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
         </select>
       </div>
-    </div>
+      <p>Years to be hidden: {hidenYears}</p>
+    </Card>
   );
 };
 
